c27431493ecb294114dd34f064ec6c5c8397aaf6
Arpit Pardhi <pardhiarpit11@gmail.com>
2021-06-10 22:05:00 +0530

    Checkpoint2

---
+++ src/common/header/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+ class Header extends Component {
+     render(){ 
+         return( 
+         <div className="header">
+            <img src="https://cdn.upgrad.com/uploads/production/286e1f11-1897-4d0c-ab0f-6b2bfc1ce642/logo.svg" className="logo" alt="logo"/>
+         </div>
+         );
+        }
+ }
+ export default Header;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Header from '../src/common/header/Header';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-  </React.StrictMode>,
+  <Header/>,
   document.getElementById('root')
 );
 
