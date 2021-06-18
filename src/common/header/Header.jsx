bfc8780cf65894798ecfa7940c028ee26d75431d
Arpit Pardhi <pardhiarpit11@gmail.com>
2021-06-18 21:03:37 +0530

    Chceckpoint5

--- src/common/header/Header.jsx
+++ src/common/header/Header.jsx
@@ -1,13 +1,15 @@
-import React, { Component } from 'react';
-import './Header.css';
+ import React, { Component } from "react";
+import "./Header.css";
+import logo from "./../../assets/logo.svg";
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <img src={logo} className="logo" alt="movies-app-logo" />
+        <div>{this.props.children}</div>
+      </div>
+    );
+  }
+}
 
- class Header extends Component {
-     render(){ 
-         return( 
-         <div className="header">
-            <img src="https://cdn.upgrad.com/uploads/production/286e1f11-1897-4d0c-ab0f-6b2bfc1ce642/logo.svg" className="logo" alt="logo"/>
-         </div>
-         );
-        }
- }
- export default Header;
+export default Header;
